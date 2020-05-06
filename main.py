3385e54ada258c6d7257f6be52dd0a77e04000b1
Mikhael <Mikhael1729@gmail.com>
2020-05-06 02:34:59 -0400

    Solve the original problem (distributing remaining elements randomly

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from random import randint
 import math
 
+def fill_group(elements_groups, current, difference, placed_elements, elements, groups_size):
+    elements_quantity = len(elements)
+
+    # Add students to the new or current group.
+    while len(elements_groups[current]) < (groups_size + difference):
+        student_is_located = True
+        all_students_are_placed = len(placed_elements) >= elements_quantity
+
+        while(student_is_located and not all_students_are_placed):
+            random_student = randint(0, elements_quantity - 1)
+
+            # Know if this selected student is placed in a group or not.
+            if placed_elements.get(random_student) == None:
+                student_is_located = False
+                placed_elements[random_student] = True
+                elements_groups[current][random_student] = elements[random_student]
+                break
+            else:
+                student_is_located = True
+
 def distribute_elements(elements, groups_size, groups_max = float('inf')):
     elements_groups = []
 
-    difference = 0 # The amount of extra elements the last group will has.
+    difference = 0 # The amount of extra elements a group will have.
     elements_quantity = len(elements)
 
     # Generate groups of elements.
@@ -15,35 +35,26 @@ def distribute_elements(elements, groups_size, groups_max = float('inf')):
         enough_to_create_new_group =  unplaced_elements >= groups_size
         exceeds_groups_max = len(elements_groups) >= groups_max
 
-        if enough_to_create_new_group and not exceeds_groups_max:
-            elements_groups.append({})
-        else:
-            difference = unplaced_elements
-
         current = len(elements_groups) - 1
 
-        # Add students to the new or current group.
-        while len(elements_groups[current]) < (groups_size + difference):
-            student_is_located = True
-            all_students_are_placed = len(placed_elements) >= elements_quantity
+        if enough_to_create_new_group and not exceeds_groups_max:
+            elements_groups.append({})
+            current += 1
+            fill_group(elements_groups, current, 0, placed_elements, elements, groups_size)    
+        else:
+            difference = 0
+            
+            for i in range(unplaced_elements):
+                random_group = randint(0, current)
+                fill_group(elements_groups, random_group, 1, placed_elements, elements, groups_size)    
 
-            while(student_is_located and not all_students_are_placed):
-                random_student = randint(0, elements_quantity - 1)
-
-                # Know if this selected student is placed in a group or not.
-                if placed_elements.get(random_student) == None:
-                    student_is_located = False
-                    placed_elements[random_student] = True
-                    elements_groups[current][random_student] = elements[random_student]
-                    break
-                else:
-                    student_is_located = True
+            break
 
     return elements_groups
 
 # Request and store the quantity of groups.
 print('N° estudiantes por grupo: ', end = '')
-groups_size = int(input())
+size = int(input())
 
 print('URL de archivo de estudiantes: ', end = '')
 students_url = str(input())
@@ -59,7 +70,8 @@ with open(students_url) as f:
 with open(topics_url) as f:
     topics = f.readlines()
 
-students_groups = distribute_elements(students, groups_size)
+# Compute groups.
+students_groups = distribute_elements(students, size)
 topic_groups = distribute_elements(topics, int(len(topics) / len(students_groups)), len(students_groups))
 
 count = 0
