ed0aa74a3ef001cba06dc9c443b12be1ae59f989
Mikhael1729 <Mikhael1729@gmail.com>
2020-06-10 00:40:16 -0400

    Make code improves and UX improves

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def fill_group(groups, group_index, elements, groups_size, difference):
         groups[group_index].append(elements[random_element])
         del elements[random_element]
 
+# Distribute the elements of a list into groups.
 def distribute_elements(elements, groups_size, groups_max = float('inf')):
     groups_max = int(len(elements) / size) if groups_max == float('inf') else groups_max
     groups = [] # Store the elements in groups.
 
     # Generate groups of elements while all elements are placed in a group.
-    while len(elements):
+    while len(elements) > 0:
         groups_quantity = len(groups)
         exceeds_groups_max = groups_quantity >= groups_max
 
@@ -30,19 +31,40 @@ def distribute_elements(elements, groups_size, groups_max = float('inf')):
                     random_group = randint(0, groups_quantity - 1)
 
                 fill_group(groups, random_group, elements, groups_size, 1)
-            break
 
     return groups
 
-# Request and store the quantity of groups.
-print('N° estudiantes por grupo: ', end = '')
-size = int(input())
+def print_list(element_list, list_name):
+    greater_than_one = True if len(element_list) > 1 else False
+    space = "     " if greater_than_one else ""
+    new_line = "\n" if greater_than_one else ""
+    s = "s" if greater_than_one else ""
 
-print('URL de archivo de estudiantes: ', end = '')
+    elements_count = 1
+    print(f'  - {list_name}{s}: {new_line}', end=new_line)
+    for value in element_list:    
+        print(f'{space}{elements_count}.', value, end = '')
+        elements_count += 1
+
+# Request and store the quantity of groups.
+print('N° estudiantes por grupo (auto): ', end = '')
+size = input()
+
+if len(size) == 0:
+  size = "auto"
+
+print('URL de archivo de estudiantes (students.txt): ', end = '')
 students_url = str(input())
 
-print('URL de archivo de temas: ', end = '')
+if len(students_url) == 0:
+  students_url = "students.txt" 
+
+print('URL de archivo de temas (topics.txt): ', end = '')
 topics_url = str(input())
+
+if len(topics_url) == 0:
+  topics_url = "topics.txt" 
+
 
 # Get the list of students.
 with open(students_url) as f:
@@ -52,28 +74,28 @@ with open(students_url) as f:
 with open(topics_url) as f:
     topics = f.readlines()
 
-# Compute groups.
-students_groups = distribute_elements(students, size)
-topic_groups = distribute_elements(topics, int(len(topics) / len(students_groups)), len(students_groups))
+# Distribute student in groups.
+size = int(size) if size.lower() != "auto" else (len(students)/len(topics))
+student_groups = distribute_elements(students, size)
 
+# Distribute topics in student groups.
+topics_groups_size = int(len(topics) / len(student_groups))
+topics_groups_max = len(student_groups)
+
+topic_groups = distribute_elements(topics, topics_groups_size, topics_groups_max)
+
+# Print the result
 print('')
 count = 0
-s_count = 0
-t_count = 0
-for i in range(len(students_groups)):
-    print(f'Grupo {count + 1}:')
+for i in range(len(student_groups)):
+    print(f'Grupo {count + 1}:  | No. Estudiantes: {len(student_groups[i])} | No. Temas: {len(topic_groups[i])}\n---')
 
-    print('  - Estudiantes')
-    for value in students_groups[i]:
-        print(f'    {s_count + 1}. ', value, end = '')
-        s_count += 1
+    print_list(student_groups[i], "Estudiante")
 
-    print('  - Temas')
-    for value in topic_groups[i]:
-        print(f'    {t_count + 1}. ', value, end = '')
-        t_count += 1
-
-    count += 1
+    print('')
+    
+    print_list(topic_groups[i], "Tema")
 
     print('')
 
+    count += 1
